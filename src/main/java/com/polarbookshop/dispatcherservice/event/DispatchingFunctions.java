d4d65361c30dd9caa7d977cb6087371ba38dcd4d
lsh <tmdgns4029@naver.com>
2024-07-07 16:36:03 +0900

    [깃워크플로] 깃 커밋해시를 아티팩트 버전으로 사용 및 수락 단계 작성

--- src/main/java/com/polarbookshop/dispatcherservice/event/DispatchingFunctions.java
+++ src/main/java/com/polarbookshop/dispatcherservice/event/DispatchingFunctions.java
@@ -6,8 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
-
 import java.util.function.Function;
+
+
 
 // 스프링 클라우드 함수는 함수를 빈으로 등록하면 인식할 수 있다.
 // 스프링 클라우드 함수는 표준 자바 함수형 인터페이스인 Function, Supplier, Consumer 를 준수한 함수를 관리한다.
